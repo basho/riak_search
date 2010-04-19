88ba27d7ce860c5c5fa7c30cabd6b786cfc52899
Kevin Smith <ksmith@basho.com>
2010-04-19 15:45:02 -0400

    Adding README and tweaking build
    --HG--
    rename : apps/basho_analyzer/java_src/analysis_server/scripts/analysis_master.sh => apps/basho_analyzer/java_src/analysis_server/scripts/analysis_server.sh

--- apps/basho_analyzer/java_src/analysis_server/scripts/analysis_master.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -server -Xms16m -Xmx64m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -cp lib/lucene-core-3.0.1.jar:lib/netty-3.1.5.GA.jar:lib/protobuf-java-2.3.0.jar:lib/analysis_master-0.1.jar com.basho.search.analysis.server.AnalysisServer 6098
