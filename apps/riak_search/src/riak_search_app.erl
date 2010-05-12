a795bb3cf419c2e0d29b4f0f2fca59794749cafe
Rusty Klophaus <rusty@basho.com>
2010-05-12 13:26:46 -0400

    Set default n_val to 2.

--- apps/riak_search/src/riak_search_app.erl
+++ apps/riak_search/src/riak_search_app.erl
@@ -30,7 +30,7 @@ start(_StartType, _StartArgs) ->
         {ok, Pid} ->
             %% Set up a riak_search bucket.
             riak_core_bucket:set_bucket(<<"search">>, [
-                {n_val, 1},
+                {n_val, 2},
                 {backend, search_backend}
             ]),
             %% Set up the search_broadcast bucket. Any operations on
