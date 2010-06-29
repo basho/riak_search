fa888f65d5ecb15e357e1b745fa8c508be0e8744
Kevin Smith <ksmith@basho.com>
2010-06-29 17:08:31 -0400

    Adding unit tests for padding logic

--- apps/riak_search_core/src/riak_search_text.erl
+++ apps/riak_search_core/src/riak_search_text.erl
@@ -2,6 +2,10 @@
 
 -export([left_pad/2]).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 left_pad([F|T]=Value, Count) ->
     [F|T] = Value,
     case F of
@@ -15,3 +19,11 @@ left_pad1(V, Count) when length(V) >= Count ->
     V;
 left_pad1(V, Count) ->
     left_pad1([$0|V], Count).
+
+-ifdef(TEST).
+padding_test() ->
+    ?assertEqual("0000000001", left_pad("1", 10)),
+    ?assertEqual("-0000000001", left_pad("-1", 10)),
+    ?assertEqual("0000001.89", left_pad("1.89", 10)),
+    ?assertEqual("-0000001.89", left_pad("-1.89", 10)).
+-endif.
