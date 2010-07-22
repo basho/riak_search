745413140533df7788db64b06f504e49a8ba6b00
Jon Meredith <jmeredith@basho.com>
2010-07-22 15:46:57 -0600

    Removed extra import statements from IntegerAnalyzerFactory.java

--- apps/qilr/java_src/analysis_server/src/com/basho/search/analysis/IntegerAnalyzerFactory.java
+++ apps/qilr/java_src/analysis_server/src/com/basho/search/analysis/IntegerAnalyzerFactory.java
@@ -8,13 +8,7 @@ package com.basho.search.analysis;
 
 import java.io.StringReader;
 
-import org.apache.lucene.analysis.LengthFilter;
-import org.apache.lucene.analysis.LowerCaseFilter;
-import org.apache.lucene.analysis.StopAnalyzer;
-import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.WhitespaceTokenizer;
-import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.util.Version;
 
 public class IntegerAnalyzerFactory implements AnalyzerFactory {
