bed79c0cc9b841c8b354728a5a542600cf8fe981
Dave Smith <dizzyd@dizzyd.com>
2010-08-02 13:50:12 -0600

    Fix bug 548; it was relatively easy to overflow 2^15 -- bumped to 2^31
    --HG--
    extra : rebase_source : dbe9f0268ac0be043d6b2348566ad44ab9eab467

--- apps/merge_index/src/mi_segment.erl
+++ apps/merge_index/src/mi_segment.erl
@@ -252,11 +252,11 @@ read_offsets(Root) ->
 
 
 read_seg_value(FH) ->
-    case file:read(FH, 2) of
-        {ok, <<1:1/integer, Size:15/integer>>} ->
+    case file:read(FH, 4) of
+        {ok, <<1:1/integer, Size:31/integer>>} ->
             {ok, <<IFT:64/unsigned>>} = file:read(FH, Size),
             {key, IFT};
-        {ok, <<0:1/integer, Size:15/integer>>} ->
+        {ok, <<0:1/integer, Size:31/integer>>} ->
             {ok, <<B/binary>>} = file:read(FH, Size),
             {value, binary_to_term(B)};
         eof ->
@@ -264,13 +264,13 @@ read_seg_value(FH) ->
     end.
 
 write_key(FH, IFT) ->
-    ok = mi_write_cache:write(FH, <<1:1/integer, 8:15/integer, IFT:64/unsigned>>),
+    ok = mi_write_cache:write(FH, <<1:1/integer, 8:31/integer, IFT:64/unsigned>>),
     10.
 
 write_seg_value(FH, Value, Props, TS) ->
     B = term_to_binary({Value, Props, TS}),
     Size = erlang:size(B),
-    ok = mi_write_cache:write(FH, <<0:1/integer, Size:15/integer, B/binary>>),
+    ok = mi_write_cache:write(FH, <<0:1/integer, Size:31/integer, B/binary>>),
     Size + 2.
 
 data_file(Segment) when is_record(Segment, segment) ->
