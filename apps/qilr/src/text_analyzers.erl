196ff3fb8c818e06361a42f4d4f196e96d79507b
Rusty Klophaus <rusty@basho.com>
2010-09-14 10:45:38 -0400

    Remove dead code.

--- apps/qilr/src/text_analyzers.erl
+++ apps/qilr/src/text_analyzers.erl
@@ -9,8 +9,6 @@
 -define(UPPERCHAR(C), (C >= $A andalso C =< $Z)).
 -define(LOWERCHAR(C), (C >= $a andalso C =< $z)).
 -define(NUMBER(C), (C >= $0 andalso C =< $9)).
--define(STOPWORD(X), (X /= "a" andalso X /= "an" andalso X /= "and" andalso X/= "are" andalso X/= "as" andalso X/= "at" andalso X/= "be" andalso X/= "but" andalso X/= "by" andalso X/= "for" andalso X/= "if" andalso X/= "in" andalso X/= "into" andalso X/= "is" andalso X/= "it" andalso X/= "no" andalso X/= "not" andalso X/= "of" andalso X/= "on" andalso X/= "or" andalso X/= "s" andalso X/= "such" andalso X/= "t" andalso X/= "that" andalso X/= "the" andalso X/= "their" andalso X/= "then" andalso X/= "there" andalso X/= "these" andalso X/= "they" andalso X/= "this" andalso X/= "to" andalso X/= "was" andalso X/= "will" andalso X/= "with")).
-
 
 %% Mimics the DefaultAnalyzerFactory.
 default_analyzer_factory(Text, [MinLength]) ->
@@ -59,59 +57,3 @@ is_stopword(Term) when length(Term) == 5 ->
     ordsets:is_element(Term, ["their", "there", "these"]);
 is_stopword(_Term) -> 
     false.
-
-%% %% Mimics the DefaultAnalyzerFactory.
-%% default_analyzer_factory(Text, Options) ->
-%%     Tokens1 = standard_tokenizer(Text, Options),
-%%     Tokens2 = length_filter(Tokens1, Options),
-%%     Tokens3 = lower_case_filter(Tokens2, Options),
-%%     Tokens4 = stop_filter(Tokens3, Options),
-%%     {ok, [list_to_binary(X) || X <- Tokens4]}.
-
-%% %% Text is binary. Return [Tokens] as a list of strings.
-%% standard_tokenizer(Text, Options) ->
-%%     Tokens = standard_tokenizer(Text, Options, []),
-%%     lists:reverse(Tokens).
-%% standard_tokenizer(<<>>, _Options, []) ->
-%%     [];
-%% standard_tokenizer(<<>>, _Options, Acc) ->
-%%     [lists:reverse(Acc)];
-%% standard_tokenizer(<<H,T/binary>>, Options, Acc) when ?UPPERCHAR(H) ->
-%%     standard_tokenizer(T, Options, [H|Acc]);
-%% standard_tokenizer(<<$.,H,T/binary>>, Options, Acc) when ?UPPERCHAR(H) ->
-%%     standard_tokenizer(T, Options, [H,$.|Acc]);
-%% standard_tokenizer(<<H,T/binary>>, Options, Acc) when ?CHAR(H) ->
-%%     standard_tokenizer(T, Options, [H|Acc]);
-%% standard_tokenizer(<<$.,H,T/binary>>, Options, Acc) when ?CHAR(H) ->
-%%     standard_tokenizer(T, Options, [H,$.|Acc]);
-%% standard_tokenizer(<<_,T/binary>>, Options, []) ->
-%%     standard_tokenizer(T, Options, []);
-%% standard_tokenizer(<<_,T/binary>>, Options, Acc) ->
-%%     [lists:reverse(Acc)|standard_tokenizer(T, Options, [])].
-
-%% %% Greater than 3 characters.
-%% %% Tokens is a list of strings.
-%% length_filter(Tokens, [MinLength]) ->
-%%     [X || X <- Tokens, length(X) >= MinLength];
-%% length_filter(Tokens, _Options) ->
-%%     [X || X <- Tokens, length(X) >= 3].
-
-%% %% Return lower case token.
-%% %% Tokens is a list of strings.
-%% lower_case_filter(Tokens, _Options) ->
-%%     [string:to_lower(X) || X <- Tokens].
-
-%% %% Filter out stopwords.
-%% %% Tokens is a list of strings.
-%% stop_filter(Tokens, Options) ->
-%%     F = fun(Token) -> 
-%%                 not is_stopword(Token, Options)
-%%         end,
-%%     lists:filter(F, Tokens).
-
-%% is_stopword(Token, _Options) -> 
-%%     Stopwords = ["a", "an", "and", "are", "as", "at", "be", "but", "by", "for", 
-%%                  "if", "in", "into", "is", "it", "no", "not", "of", "on", "or", 
-%%                  "s", "such", "t", "that", "the", "their", "then", "there", 
-%%                  "these", "they", "this", "to", "was", "will", "with"],
-%%     lists:member(Token, Stopwords).
