9f37bdf1da98f2b7ba41f0a567177f035d0ccc7c
Rusty Klophaus <rusty@basho.com>
2010-09-15 08:31:56 -0400

    Update text_signature and hash_signature to work with both binaries and integer term values.

--- apps/merge_index/src/mi_utils.erl
+++ apps/merge_index/src/mi_utils.erl
@@ -37,33 +37,37 @@ longest_prefix_1(_, _) ->
 %% edit_signature/2 - Given an A term and a B term, return a bitstring
 %% consisting of a 0 bit for each matching char and a 1 bit for each
 %% non-matching char.
-edit_signature(A, B) ->
-    list_to_bitstring(edit_signature_1(A, B)).
-edit_signature_1(<<C, A/binary>>, <<C, B/binary>>) ->
-    [<<0:1/integer>>|edit_signature_1(A, B)];
-edit_signature_1(<<_, A/binary>>, <<_, B/binary>>) ->
-    [<<1:1/integer>>|edit_signature_1(A, B)];
-edit_signature_1(<<>>, <<_, B/binary>>) ->
-    [<<1:1/integer>>|edit_signature_1(<<>>, B)];
-edit_signature_1(<<_/binary>>, <<>>) ->
-    [];
-edit_signature_1(<<>>, <<>>) ->
-    [].    
+edit_signature(A, B) when is_binary(A) andalso is_binary(B) ->
+    list_to_bitstring(edit_signature_binary(A, B));
+edit_signature(A, B) when is_integer(A) ->
+    edit_signature(<<A:32/integer>>, B);
+edit_signature(A, B) when is_integer(B) ->
+    edit_signature(A, <<B:32/integer>>).
+
+edit_signature_binary(<<C, A/binary>>, <<C, B/binary>>) ->
+    [<<0:1/integer>>|edit_signature_binary(A, B)];
+edit_signature_binary(<<_, A/binary>>, <<_, B/binary>>) ->
+    [<<1:1/integer>>|edit_signature_binary(A, B)];
+edit_signature_binary(<<>>, <<_, B/binary>>) ->
+    [<<1:1/integer>>|edit_signature_binary(<<>>, B)];
+edit_signature_binary(_, <<>>) ->
+    [].
 
 %% hash_signature/1 - Given a term, repeatedly xor and rotate the bits
 %% of the field to calculate a unique 1-byte signature. This is used
 %% for speedier matches.
-hash_signature(Term) ->
-    hash_signature(Term, 0).
-hash_signature(<<C, Rest/binary>>, Acc) ->
+hash_signature(Term) when is_binary(Term)->
+    hash_signature_binary(Term, 0);
+hash_signature(Term) when is_integer(Term) ->
+    hash_signature(<<Term:64/integer>>).
+hash_signature_binary(<<C, Rest/binary>>, Acc) ->
     case Acc rem 2 of
         0 -> 
             RotatedAcc = ((Acc bsl 1) band 255),
-            hash_signature(Rest, RotatedAcc bxor C);
+            hash_signature_binary(Rest, RotatedAcc bxor C);
         1 -> 
             RotatedAcc = (((Acc bsl 1) + 1) band 255),
-            hash_signature(Rest, RotatedAcc bxor C)
+            hash_signature_binary(Rest, RotatedAcc bxor C)
     end;
-hash_signature(<<>>, Acc) ->
+hash_signature_binary(<<>>, Acc) ->
     Acc.
-    
