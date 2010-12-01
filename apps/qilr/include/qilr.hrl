cdd2da4666c1feaf91f2c58fe1c35bdbc235c2b8
Rusty Klophaus <rklophaus@gmail.com>
2010-12-01 17:30:04 -0500

    Updated qilr to only start the Java-analyzer if the analysis port is defined. Added erlang implementations of analyzers for whitespace, integer, and noop (which just reflects the field w/out tokenization.) Updated test scripts to use the new erlang-based analyzers. Fixed bug causing padding_size to be ignored.

--- apps/qilr/include/qilr.hrl
+++ apps/qilr/include/qilr.hrl
@@ -1,4 +1,9 @@
--define(WHITESPACE_ANALYZER, "com.basho.search.analysis.WhitespaceAnalyzerFactory").
--define(INTEGER_ANALYZER,    "com.basho.search.analysis.IntegerAnalyzerFactory").
+-define(WHITESPACE_ANALYZER, {erlang, text_analyzers, whitespace_analyzer_factory}).
+-define(INTEGER_ANALYZER,    {erlang, text_analyzers, integer_analyzer_factory}).
+-define(NOOP_ANALYZER,       {erlang, text_analyzers, noop_analyzer_factory}).
 
 -define(CONN_POOL, qilr_conn_pool).
+
+-ifndef(PRINT).
+-define(PRINT(Var), io:format("DEBUG: ~p:~p - ~p~n~n ~p~n~n", [?MODULE, ?LINE, ??Var, Var])).
+-endif.
