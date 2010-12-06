a3e13213a526a74a41f362d7db5145e3d08da83c
Rusty Klophaus <rklophaus@gmail.com>
2010-12-06 11:01:16 -0500

    Update #scope{} operator to correctly rewrite the query during preplanning. Remove duplicate code in #group{} operator.

--- apps/riak_search/src/riak_search_op_group.erl
+++ apps/riak_search/src/riak_search_op_group.erl
@@ -15,6 +15,9 @@
 
 preplan(Op, State) -> 
     case Op#group.ops of
+        SingleOp when is_tuple(SingleOp) ->
+            %% If there is only one op, no need to wrap it...
+            NewOp = SingleOp;
         [SingleOp] ->
             %% If there is only one op, no need to wrap it...
             NewOp = SingleOp;
@@ -44,35 +47,7 @@ preplan(Op, State) ->
     end,
     riak_search_op:preplan(NewOp, State).
 
-chain_op(Op, OutputPid, OutputRef, State) ->
-    case Op#group.ops of
-        [SingleOp] ->
-            %% If there is only one op, no need to wrap it...
-            NewOp = SingleOp;
-        OpList ->
-            %% Otherwise, look at schema to figure out if our default combine
-            %% operation is AND or OR, and call intersection or union
-            %% appropriately.
-            Index = State#search_state.index,
-            {ok, Schema} = riak_search_config:get_schema(Index),
-            case Schema:default_op() of
-                'and' ->
-                    NewOp = #intersection { id=Op#group.id, ops=OpList };
-                'or' ->
-                    %% In this case, check if there are any
-                    %% negated/prohibited terms. If so, then rewrite
-                    %% the query so that the negated terms subtract
-                    %% from the overall results.
-                    F = fun(X) -> is_record(X, negation) end,
-                    {NegatedOps, NormalOps} = lists:partition(F, Op#group.ops),
-                    NegatedOps1 = [X#negation.op || X <- NegatedOps],
-
-                    NewOp = #intersection { id=Op#group.id, ops=[
-                        #union { id=Op#group.id, ops=NormalOps },
-                        #negation { id=Op#group.id, op=#union { id=Op#group.id, ops=NegatedOps1 } }
-                    ]}
-            end
-    end,
-
-    %% Call the new operation...
-    riak_search_op:chain_op(NewOp, OutputPid, OutputRef, State).
+chain_op(Op, _OutputPid, _OutputRef, _State) ->
+    %% Any #group{} operators should get rewritten to an
+    %% #intersection{} or #union{} operator above.
+    throw({invalid_query_tree, Op}).
