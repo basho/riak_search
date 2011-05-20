basho/riak_search

commit 78bc13489943c3f8b4773d89ff592698a5939552

Add riak_search_utils:ptransform/N function as a replacement to plists:map/N. plists:map/N had some bugs around messages being received by processes that were not intended to receive them.

apps/riak_search/src/riak_search_utils.erl
@@ -21,10 +21,12 @@
     coalesce/1, coalesce/2,
     binary_inc/2,
     ets_keys/1,
-    consult/1
+    consult/1,
+    ptransform/2
 ]).
 
 -include("riak_search.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 %% Given to terms, combine the properties in some sort of reasonable
 %% way. This basically means concatenating the score and the word list
@@ -172,4 +174,73 @@ consult_2(AST) ->
             Error
     end.
 
+
+%% Run a transform operation in parallel. Results are returned as a
+%% list, ordering is not guaranteed in any way. This was implemented
+%% as a simple substitute to the plists.erl module. The plists module
+%% has some subtle bugs because messages are not tagged with
+%% Refs. This causes heisenbugs.
+ptransform(F, List) ->
+    Schedulers = erlang:system_info(schedulers),
+    ptransform(F, List, Schedulers).
+
+%% Run a map operation in parallel.
+ptransform(F, List, NumProcesses) ->
+    %% Calculate our batch size by dividing the size of the list by
+    %% the number of processes. Batch size should be at least 1.
+    ListLength = length(List),
+    BatchSize = lists:max([1, ListLength div NumProcesses]),
+    
+    %% Create a ref, used to prevent later interference.
+    Ref = make_ref(),
+    Pids = ptransform_spawn(F, List, ListLength, Ref, BatchSize, []),
+    ptransform_collect(Ref, Pids, []).
+
+ptransform_spawn(F, List, ListLength, Ref, BatchSize, Pids) when List /= [] ->
+    %% Get the next BatchSize items from list, spawn a map that sends
+    %% results back to the collector.
+    case ListLength < BatchSize of
+        true ->
+            {Pre, Post} = {List, []},
+            NewListLength = 0;
+        false -> 
+            {Pre, Post} = lists:split(BatchSize, List),
+            NewListLength = ListLength - BatchSize
+    end,
+
+    %% Spawn up a worker for this chunk.
+    Parent = self(),
+    SpawnF = fun() ->
+                     Results = lists:map(F, Pre),
+                     Parent ! {results, Results, self(), Ref}
+             end,
+    Pid = erlang:spawn_link(SpawnF),
+    ptransform_spawn(F, Post, NewListLength, Ref, BatchSize, [Pid|Pids]);
+ptransform_spawn(_, [], 0, _, _, Pids) ->
+    %% No more items left in list, return Pids.
+    Pids.
+
+ptransform_collect(Ref, Pids, Acc) when Pids /= [] ->
+    %% Collect a chunk, and concat results.
+    receive 
+        {results, Results, Pid, Ref} ->
+            NewPids = Pids -- [Pid],
+            NewAcc = Results ++ Acc,
+            ptransform_collect(Ref, NewPids, NewAcc)
+    end;
+ptransform_collect(_, [], Acc) ->
+    %% We've read from all the pids, so return.
+    Acc.
+
+ptransform_test() ->
+    Test = fun(List) ->
+                   F = fun(X) -> X * 2 end,
+                   ?assertEqual(lists:sort(ptransform(F, List)), lists:map(F, List))
+           end,
+    Test(lists:seq(0, 0)),
+    Test(lists:seq(1, 1)),
+    Test(lists:seq(1, 2)),
+    Test(lists:seq(1, 3)),
+    Test(lists:seq(1, 20)),
+    Test(lists:seq(1, 57)).
 

apps/riak_search/ebin/riak_search.app
@@ -4,7 +4,6 @@
   {vsn, "0.13.0"},
   {modules, [
              search,
-             plists,
              riak_indexed_doc,
              riak_search,
              riak_search_cmd,

apps/riak_search/src/riak_search_client.erl
@@ -98,7 +98,7 @@ search_doc(IndexOrSchema, QueryOps, QueryStart, QueryRows, Timeout) ->
     F = fun({Index, DocID, _}) ->
         riak_indexed_doc:get(RiakClient, Index, DocID)
     end,
-    Documents = plists:map(F, Results, {processes, 4}),
+    Documents = riak_search_utils:ptransform(F, Results),
     {Length, MaxScore, [X || X <- Documents, X /= {error, notfound}]}.
 
 %% Index a specified #riak_idx_doc
@@ -245,7 +245,7 @@ process_terms_1(IndexFun, BatchSize, PreflistCache, Terms) ->
                      IndexFun(VNode, Postings)
              end,
         VNodes = riak_search_utils:ets_keys(ReplicaTable),
-        plists:map(F3, VNodes, {processes, 4}),
+        riak_search_utils:ptransform(F3, VNodes),
 
         %% Repeat until there are no more terms...
         process_terms_1(IndexFun, BatchSize, NewPreflistCache, Rest)

commit 66bc9aa0950008ea44e61f49a1621fe6ab3d3c53

Maintain sort order while performing search result doc lookups.
This fixes a bug introduced in commit 78bc1348. We replaced plists:map/N with the ptransform/2 function, which unfortunately no longer preserved the order of the original list of items to transform. Modified ptransform/2 to preserve the original order.

apps/riak_search/src/riak_search_utils.erl
@@ -186,8 +186,17 @@ consult_2(AST) ->
 %% has some subtle bugs because messages are not tagged with
 %% Refs. This causes heisenbugs.
 ptransform(F, List) ->
+    %% Maintain order by adding a position to the list. Then run the
+    %% results, sort, and return the unwrapped list.
+    WrappedF = fun({Pos, X}) -> {Pos, F(X)} end,
+    WrappedList = lists:zip(lists:seq(1, length(List)), List),
+
+    %% Run in parallel for however many schedulers there are.
     Schedulers = erlang:system_info(schedulers),
-    ptransform(F, List, Schedulers).
+    Results = ptransform(WrappedF, WrappedList, Schedulers),
+
+    %% Unwrap and return the results.
+    [X || {_,X} <- lists:sort(Results)].
 
 %% Run a map operation in parallel.
 ptransform(F, List, NumProcesses) ->