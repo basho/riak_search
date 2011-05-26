2dbd5a27cb9ae157646ddebe5431f0f6c86229a9
Rusty Klophaus <rklophaus@gmail.com>
2011-05-26 16:11:35 -0400

    Update riak_search mapreduce interface to accept an optional filter, allowing users to limit a query based on inline fields.
    Fixes: bz1110 az446
    
    Commit fd275e8c introduced inline field support, giving users a new way
    to filter search results. That change unfortunately was only applied to
    the Solr interface, and it broke the MapReduce interface. This change
    fixes the MapReduce interface, and also adds the ability to do filtering
    via the MapReduce interface.
    
    Previously, a developer could execute a MapReduce operation using inputs
    from RiakSearch by sending in the following tuple as an input:
    `{modfun, riak_search, mapred_search, [Bucket, Search]}`.
    
    With this change, the developer can also apply a filter, ie:
    `{modfun, riak_search, mapred_search, [Bucket, Search, Filter]}`.
    
    Conflicts:
    
    	src/riak_search_client.erl

--- src/riak_search.erl
+++ src/riak_search.erl
@@ -38,20 +38,32 @@ analyze(Text, {erlang, Mod, Fun}, AnalyzerArgs) ->
     Mod:Fun(Text, AnalyzerArgs).
 
 %% Used in riak_kv Map/Reduce integration.
-mapred_search(FlowPid, Options, Timeout) ->
-    %% Get the Index and Query from properties...
-    [DefaultIndex, Query] = Options,
+mapred_search(FlowPid, [DefaultIndex, Query], Timeout) ->
+    mapred_search(FlowPid, [DefaultIndex, Query, ""], Timeout);
+    
+mapred_search(FlowPid, [DefaultIndex, Query, Filter], Timeout) ->
+    {ok, Client} = riak_search:local_client(),
 
     %% Parse the query...
-    {ok, Client} = riak_search:local_client(),
     case Client:parse_query(DefaultIndex, Query) of
-        {ok, Ops} ->
-            QueryOps = Ops;
-        {error, ParseError} ->
-            M = "Error running query '~s': ~p~n",
-            error_logger:error_msg(M, [Query, ParseError]),
-            throw({mapred_search, Query, ParseError}),
+        {ok, Ops1} ->
+            QueryOps = Ops1;
+        {error, ParseError1} ->
+            M1 = "Error running query '~s': ~p~n",
+            error_logger:error_msg(M1, [Query, ParseError1]),
+            throw({mapred_search, Query, ParseError1}),
             QueryOps = undefined % Make compiler happy.
+    end,
+
+    %% Parse the filter...
+    case Client:parse_filter(DefaultIndex, Filter) of
+        {ok, Ops2} ->
+            FilterOps = Ops2;
+        {error, ParseError2} ->
+            M2 = "Error running query '~s': ~p~n",
+            error_logger:error_msg(M2, [Filter, ParseError2]),
+            throw({mapred_search, Filter, ParseError2}),
+            FilterOps = undefined % Make compiler happy.
     end,
 
     %% Perform a search, funnel results to the mapred job...
@@ -62,6 +74,6 @@ mapred_search(FlowPid, Options, Timeout) ->
         luke_flow:add_inputs(FlowPid, BKeys),
         Acc
     end,
-    ok = Client:search_fold(DefaultIndex, QueryOps, F, ok, Timeout),
+    ok = Client:search_fold(DefaultIndex, QueryOps, FilterOps, F, ok, Timeout),
     luke_flow:finish_inputs(FlowPid).
 
