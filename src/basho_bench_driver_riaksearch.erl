dde732e6ff6fa8c1a846a64d85ab4ae7c29dc984
rzezeski <rzezeski@gmail.com>
2011-07-15 01:59:18 -0400

    Add ability to run static query with bench driver
    I wanted the ability to bench a static query against a prepopulated
    Search cluster.  This is now possible by binding a list to the
    `riaksearch_query` config.  It should be a list of 2-3 args
    
    [Index, Query, Filter]
    
    e.g. ["tweets", "created_at:[20100113T032200 TO 20100113T032500]",
          "text":"earthquake"]
    
    Furthermore, you can also set `riaksearch_expected` to an integer
    and it will check to make sure the correct number of results were
    returned.

--- src/basho_bench_driver_riaksearch.erl
+++ src/basho_bench_driver_riaksearch.erl
@@ -3,19 +3,17 @@
 %% Copyright (c) 2007-2010 Basho Technologies, Inc.  All Rights Reserved.
 %%
 %% -------------------------------------------------------------------
-
 -module(basho_bench_driver_riaksearch).
 
 -export([
     new/1,
     run/4,
     valgen/3,
-    file_to_array/1, file_to_array/2 % Get rid of compiler warnings.
+    file_to_array/2
 ]).
 
--record(state, { nodes, fields, terms, queries }).
--define(PRINT(Var), io:format("DEBUG: ~p:~p - ~p~n~n ~p~n~n", [?MODULE, ?LINE, ??Var, Var])).
-
+-define(FMT(Str, Args), lists:flatten(io_lib:format(Str, Args))).
+-record(state, { nodes, fields, terms, queries, 'query', expected}).
 
 %% ====================================================================
 %% API
@@ -23,7 +21,7 @@
 
 new(_Id) ->
     MyNode = basho_bench_config:get(riaksearch_node),
-    Cookie = basho_bench_config:get(riaksearch_cookie),
+    Cookie = basho_bench_config:get(riaksearch_cookie, riak),
     net_kernel:start([MyNode]),
     erlang:set_cookie(node(), Cookie),
 
@@ -36,23 +34,27 @@ new(_Id) ->
     [F(X) || X <- Nodes],
 
     %% Load the field array...
-    FieldFile = basho_bench_config:get(riaksearch_fieldfile),
+    FieldFile = basho_bench_config:get(riaksearch_fieldfile, undefined),
     %% FieldArray = file_to_array(FieldFile, 10),
     FieldArray = file_to_array(FieldFile),
     
     %% Load the word array...
-    TermFile = basho_bench_config:get(riaksearch_termfile),
+    TermFile = basho_bench_config:get(riaksearch_termfile, undefined),
     %% TermArray = file_to_array(TermFile, 100),
     TermArray = file_to_array(TermFile),
+
+    Query = basho_bench_config:get(riaksearch_query, undefined),
+    E = basho_bench_config:get(riaksearch_expected, undefined),
     
     State = #state { 
         nodes=Nodes,
         fields=FieldArray, 
         terms=TermArray,
-        queries=queue:new()
+        queries=queue:new(),
+        'query'=Query,
+        expected=E
     },
     {ok, State}.
-
 
 run('index', KeyGen, ValueGen, State) ->
     %% Make the index call...
@@ -79,6 +81,24 @@ run('index', KeyGen, ValueGen, State) ->
             NewQueries = queue:in({QueryField, QueryTerm}, State#state.queries),
             {ok, State#state { queries=NewQueries }}
     end;
+
+run(search, _KeyGen, _ValueGen,
+    S=#state{'query'=Query, expected=E, nodes=Nodes})
+  when Query /= undefined ->
+    %% Query will be a list of 1-3 args [Index, Query, Filter]
+    %% e.g. ["index", "field:bar", "text:hello"]
+    Node = choose(Nodes),
+    Args = lists:map(fun list_to_binary/1, Query),
+    {N, _} = rpc:call(Node, search, search, Args),
+    case E of
+        undefined -> {ok, S};
+        _ ->
+            if N == E -> {ok, S};
+               true ->
+                    {error, ?FMT("Expected ~p but received ~p", [E, N]), S}
+            end
+    end;
+
 run(search, _KeyGen, _ValueGen, State) ->
     case queue:out(State#state.queries) of
         {{value, {QueryField, QueryTerm}}, NewQueries} ->
@@ -107,6 +127,9 @@ file_to_array(FilePath, Limit) ->
         false -> 
             Words
     end.
+
+file_to_array(undefined) ->
+    [];
 file_to_array(FilePath) ->
     case file:read_file(FilePath) of
         {ok, Bytes} ->
