51ec11eeb465ca3ddfedb40ab8e1f2552e4a9585
Ryan Zezeski <rzezeski@gmail.com>
2012-01-05 20:42:44 -0500

    Remove indexing via Solr-like interface, only index KV objects

--- src/riak_search_app.erl
+++ src/riak_search_app.erl
@@ -37,10 +37,6 @@ start(_StartType, _StartArgs) ->
                     Root = app_helper:get_env(riak_solr, solr_name, "solr"),
                     case riak_solr_sup:start_link() of
                         {ok, _} ->
-                            webmachine_router:add_route({[Root, index, "update"],
-                                                         riak_solr_indexer_wm, []}),
-                            webmachine_router:add_route({[Root, "update"],
-                                                         riak_solr_indexer_wm, []}),
                             webmachine_router:add_route({[Root, index, "select"],
                                                          riak_solr_searcher_wm, []}),
                             webmachine_router:add_route({[Root, "select"],
