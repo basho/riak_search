2f9f922ede55dd5f00cc78bf692aa45809b1b188
Ryan Zezeski <rzezeski@gmail.com>
2012-01-11 23:10:33 -0500

    Fill out search op, assume 1 term for now

--- src/rs_bb_driver.erl
+++ src/rs_bb_driver.erl
@@ -9,21 +9,38 @@
 -export([terms_from_file/4]).
 
 -include_lib("basho_bench/include/basho_bench.hrl").
--record(state, {urls}).
+-record(state, {i, urls}).
 
 %% ====================================================================
 %% API
 %% ====================================================================
 
 new(_Id) ->
+    ibrowse:start(),
     Ports = basho_bench_config:get(rs_ports, [{"127.0.0.1", 8098}]),
     Path = basho_bench_config:get(rs_path, "/search/test"),
-    URLs = lists:map(make_url(Path), Ports),
-    {ok, #state{urls=URLs}}.
+    URLs = array:from_list(lists:map(make_url(Path), Ports)),
+    N = basho_bench_config:get(concurrent),
+    {ok, #state{i={0, N-1}, urls=URLs}}.
 
-run(search, KeyGen, _ValGen, S=#state{urls=URLs}) ->
-    K = KeyGen(),
-    {ok, S}.
+%% steps:
+%% 1. get URL for index I
+%% 2. get field/term pair
+%% 3. build query str
+%% 4. append query str to URL
+%% 5. send HTTP req to URL
+%% 6. verify 200 or log error
+run(search, KeyGen, _ValGen, S=#state{i={Idx,_}=I, urls=URLs}) ->
+    Base = array:get(Idx, URLs),
+    {Field, [Term]} = KeyGen(),
+    Qry = ?FMT("~s:~s", [Field, Term]),
+    Params = mochiweb_util:urlencode([{<<"q">>, Qry}]),
+    URL = ?FMT("~s~s", [Base, Params]),
+    S2 = S#state{i=wrap(I)},
+    case http_get(URL) of
+        {ok, _, _} -> {ok, S2};
+        {error, Reason} -> {error, Reason, S2}
+    end.
 
 %% ====================================================================
 %% Key Gens
@@ -53,3 +70,13 @@ terms_from_file(Id, Path, Fields, Schema) ->
 
 make_url(Path) ->
     fun({IP, Port}) -> ?FMT("http://~s:~w~s", [IP, Port, Path]) end.
+
+http_get(URL) ->
+    case ibrowse:send_req(URL, [], get) of
+        {ok, "200", _, _} -> ok;
+        {ok, Status, _, _} -> {error, {bad_status, Status, URL}};
+        {error, Reason} -> {error, Reason}
+    end.
+
+wrap({I,N}) when I == N -> {0,N};
+wrap({I,N}) -> {I+1,N}.
