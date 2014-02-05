7e57044e94eb414fc25b5c12177cc64bdca9653a
Eric Redmond <eric.redmond@gmail.com>
2014-02-05 13:38:37 -0800

    Add deprecation notice on Riak Search startup

--- src/riak_search_app.erl
+++ src/riak_search_app.erl
@@ -20,6 +20,9 @@
 start(_StartType, _StartArgs) ->
     case app_helper:get_env(riak_search, enabled, false) of
         true ->
+            lager:warning("This version of Riak Search has been deprecated. "
+                "Please upgrade to Yokozuna"),
+
             %% Ensure that the KV service has fully loaded.
             riak_core:wait_for_service(riak_kv),
 
